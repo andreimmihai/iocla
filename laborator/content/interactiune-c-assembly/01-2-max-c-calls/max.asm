64390e135d982fb412a8fb2aadfcd866a0a5fc66
Andrei-Claudiu Mihai <andreiimmihai@gmail.com>
2024-05-16 22:23:02 +0300

    lab1: ex1-2: Add solutions for exercises 1 and 2
    - I defined a global variable "pos", where pos is the position of the
    maximum value in the array
    - I modified the assembly code to accept the additional argument of the
    get_max function for the position and update it with the index of the
    maximum value
    
    Signed-off-by: Andrei-Claudiu Mihai <andreiimmihai@gmail.com>

--- laborator/content/interactiune-c-assembly/01-2-max-c-calls/max.asm
+++ laborator/content/interactiune-c-assembly/01-2-max-c-calls/max.asm
@@ -10,21 +10,40 @@ get_max:
 
 	; save ebx in callee
 	push ebx
+	push esi
 
 	; [ebp+8] is array pointer
 	; [ebp+12] is array length
+	; [epb+16] is pos pointer
 
 	mov ebx, [ebp+8]
 	mov ecx, [ebp+12]
+	mov esi, [ebp+16]
 	xor eax, eax
+	xor edx, edx ; edx = max index
+	xor edi, edi ; edi = current index
 
 compare:
-	cmp eax, [ebx+ecx*4-4]
+	; cmp eax, [ebx+ecx*4-4]
+	; jge check_end
+	; mov eax, [ebx+ecx*4-4]
+	cmp edi, ecx
 	jge check_end
-	mov eax, [ebx+ecx*4-4]
-check_end:
-	loopnz compare
 
+	mov edx, [ebx + edi*4]
+	cmp eax, edx
+	jge skip_update
+
+	mov eax, edx
+	mov [esi], edi ; update max index
+
+skip_update:
+	inc edi
+	jmp compare
+
+check_end:
+	;loopnz compare
+	pop esi
 	pop ebx
 
 	leave
